b9fdc3373b35eb606dcb3d68ac06416974428cf7
Richa-iitr <richa@cs.iitr.ac.in>
2023-02-03 15:26:51 +0530

    minor changes

--- packages/contracts/src/Factory.sol
+++ packages/contracts/src/Factory.sol
@@ -3,11 +3,14 @@ pragma solidity ^0.8.0;
 
 import 'openzeppelin-contracts/proxy/Clones.sol';
 
+// 0xE42289016E024F3F322896C6728f0434545465C1
 interface Account {
   function addModule(bytes4[] memory sigs, address module) external;
+
+  function initialize(address factory_, address owner_) external;
 }
 
-// 0x65eca34df2343f360cc9be5c3445a8092a0343b3
+//
 
 contract AccountFactory {
   event cloneCreated(address clone);
@@ -19,6 +22,7 @@ contract AccountFactory {
     bytes4[][] memory sigs_
   ) public returns (address instance) {
     instance = Clones.clone(_implementation);
+    Account(instance).initialize(address(this), msg.sender);
     emit cloneCreated(instance);
 
     uint256 len_ = modules_.length;
