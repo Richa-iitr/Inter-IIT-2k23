32095705dff586c33ae9c94e9d77d7d760f0720f
Richa-iitr <richa@cs.iitr.ac.in>
2023-02-04 15:28:37 +0530

    deployed

--- packages/contracts/src/modules/SafeSwap/SafeSwap.sol
+++ packages/contracts/src/modules/SafeSwap/SafeSwap.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.0;
 
 import {IParaswap, IERC20, IUniswapV2Factory, IUniswapV2Router02} from './Interfaces.sol';
 
-//GOERLI: 0x517F886127DF76E61640c7d6C9AFd515bf757220
+//GOERLI: 0x89012390386aD3337dDd6735B9EAe5e2FAACb21f
 contract SwapEvents {
   event SwappedWith1Inch(
     address tokenIn,
