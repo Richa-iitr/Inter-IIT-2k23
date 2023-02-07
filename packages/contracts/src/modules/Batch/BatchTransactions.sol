2ce1d2be5fedd90b9bfa33b18b8bd70b3d44f5b4
Richa-iitr <richa@cs.iitr.ac.in>
2023-02-07 08:42:04 +0530

    added new deployments

--- packages/contracts/src/modules/Batch/BatchTransactions.sol
+++ packages/contracts/src/modules/Batch/BatchTransactions.sol
@@ -3,8 +3,42 @@
 pragma solidity ^0.8.0;
 
 // 0xaca091817aa8fd7863833fea1bf9f8f500eaf795
+
+interface IERC20 {
+  function transferFrom(
+    address sender,
+    address recipient,
+    uint256 amount
+  ) external returns (bool);
+
+  function approve(address spender, uint256 amount) external returns (bool);
+}
+
+interface IAavePool {
+  function deposit(
+    address asset,
+    uint256 amount,
+    address onBehalfOf,
+    uint16 referralCode
+  ) external;
+
+  function borrow(
+    address asset,
+    uint256 amount,
+    uint256 interestRateMode,
+    uint16 referralCode,
+    address onBehalfOf
+  ) external;
+}
+
 contract BatchTransaction {
   event TransactionComplete(address to_, bytes data_);
+  event LeverageDone(
+    address supplyToken,
+    address borrowToken,
+    uint256 supplyAmount,
+    uint256 borrowAmount
+  );
 
   function sendBatchedTransactions(
     address[] memory _address,
